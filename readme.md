252425764b131b0e628b2debd34a70d1232cac0a
o7-machinehum <ryan.cjw@gmail.com>
2026-03-07 15:47:56 +0100

    Inital commit, MSD working.

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Ovrdrive v2
+Firmware for Ovrdrive v2 - the worlds only self-destructing USB drive.
+
+## Getting Started
+``` bash
+git submodule update --init --recursive --checkout --force
+cd wch-ch56x-isp
+make # Build the ISP tool
+```
+
+## Install the toolchain
+The HydraUSB3 project requires a patched GCC that supports the
+`WCH-Interrupt-fast` interrupt attribute. Download the xpack GCC from the HydraUSB3 project:
+
+```bash
+cd /tmp
+curl -sL -o riscv-gcc-xpack.tar.gz \
+  "https://github.com/hydrausb3/riscv-none-elf-gcc-xpack/releases/download/12.2.0-1/xpack-riscv-none-elf-gcc-12.2.0-1-linux-x64.tar.gz"
+tar xzf riscv-gcc-xpack.tar.gz
+sudo cp -r xpack-riscv-none-elf-gcc-12.2.0-1/* /usr/local/
+```
+
+## Build the project
+```
+make
+make flash
+```
---
+++ src/main.c
@@ -0,0 +1,120 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : Main.c
+* Description        : USB Mass Storage Device (MSC/BOT) using HydraUSB3 BSP
+*                      USB2 High Speed and USB3 Super Speed
+*                      Dummy RAM-backed storage for stack validation
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************/
+#include "CH56x_common.h"
+#include "CH56x_debug_log.h"
+
+#include "CH56x_usb30_devbulk.h"
+#include "CH56x_usb20_devbulk.h"
+#include "CH56x_usb30_devbulk_LIB.h"
+#include "CH56x_usb_devbulk_desc_cmd.h"
+
+#include "sw_udisk.h"
+
+#undef FREQ_SYS
+/* System clock / MCU frequency in Hz */
+#define FREQ_SYS (120000000)
+
+#if(defined DEBUG)
+#define UART1_BAUD (5000000)
+#endif
+
+/* Blink time in ms */
+#define BLINK_FAST (50)
+#define BLINK_USB3 (250)
+#define BLINK_USB2 (500)
+
+int blink_ms = BLINK_USB2;
+
+debug_log_buf_t log_buf;
+
+/* FLASH_ROMA Read Unique ID (8bytes/64bits) */
+#define FLASH_ROMA_UID_ADDR (0x77fe4)
+usb_descriptor_serial_number_t unique_id;
+
+/* USB VID PID - use default from BSP */
+usb_descriptor_usb_vid_pid_t vid_pid =
+{
+	.vid = { .id_16b = 0x16C0 },
+	.pid = { .id_16b = 0x05DC }
+};
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main()
+{
+	/* HydraUSB3 configure GPIO In/Out */
+	bsp_gpio_init();
+
+	/* Init BSP (MCU Frequency & SysTick) */
+	bsp_init(FREQ_SYS);
+	log_init(&log_buf);
+
+#if(defined DEBUG)
+	/* Configure serial debugging for printf()/log_printf()... */
+	UART1_init(UART1_BAUD, FREQ_SYS);
+#endif
+	log_printf("HydraUSB3_MSC Start\n");
+	log_printf("ChipID(Hex)=%02X\n", R8_CHIP_ID);
+
+	memset(&unique_id, 0, 8);
+	FLASH_ROMA_READ(FLASH_ROMA_UID_ADDR, (uint32_t*)&unique_id, 8);
+
+	log_printf("HydraUSB3_MSC FW v0.1 (CPU Freq=%d MHz)\n", (FREQ_SYS/1000000));
+
+	// USB2 & USB3 Init
+	R32_USB_CONTROL = 0;
+	PFIC_EnableIRQ(USBSS_IRQn);
+	PFIC_EnableIRQ(LINK_IRQn);
+
+	PFIC_EnableIRQ(TMR0_IRQn);
+	R8_TMR0_INTER_EN = RB_TMR_IE_CYC_END;
+	TMR0_TimerInit(67000000); // USB3.0 connection failure timeout about 0.56 seconds
+
+	/* USB Descriptor set String Serial Number with CH569 Unique ID */
+	usb_descriptor_set_string_serial_number(&unique_id);
+
+	/* USB Descriptor set USB VID/PID */
+	usb_descriptor_set_usb_vid_pid(&vid_pid);
+
+	/* USB3.0 initialization */
+	USB30D_init(ENABLE);
+
+	/* Enable Udisk with dummy capacity */
+	Udisk_Capability = 1024; /* 1024 sectors * 512 bytes = 512KB dummy disk */
+	Udisk_Status |= DEF_UDISK_EN_FLAG;
+
+	log_printf("MSC ready, entering main loop\n");
+
+	// Infinite loop - MSC protocol handled via interrupts + main loop polling
+	while(1)
+	{
+		UDISK_onePack_Deal();
+	}
+}
+
+/*********************************************************************
+ * @fn      HardFault_Handler
+ *
+ * @brief   HardFault Handler
+ *
+ * @return  none
+ */
+__attribute__((interrupt("WCH-Interrupt-fast"))) void HardFault_Handler(void)
+{
+	printf("HardFault\n");
+	printf(" SP=0x%08X\n", __get_SP());
+	printf(" MIE=0x%08X\n", __get_MIE());
+	printf(" MSTATUS=0x%08X\n", __get_MSTATUS());
+	printf(" MCAUSE=0x%08X\n", __get_MCAUSE());
+	bsp_wait_ms_delay(1);
+}
