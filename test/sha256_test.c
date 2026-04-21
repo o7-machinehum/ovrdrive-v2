1633d424bb9246cb29cec9b7ccad3d74d9f5f010
o7-machinehum <ryan.cjw@gmail.com>
2026-04-21 20:51:59 +0200

    Added tests to test our sha256 function

---
+++ test/sha256_test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/sha.h>
+#include <string.h>
+
+#include "../src/crypto.h"
+
+#define BOLDRED     "\033[1m\033[31m"
+#define BOLDGREEN   "\033[1m\033[32m"
+
+// Print hash
+void print_sha256(unsigned char* hash) {
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        printf("%02x", hash[i]);
+    }
+
+    printf("\n");
+}
+
+// Test equality of two hash's
+bool test_sha256(unsigned char* hash1, unsigned char* hash2) {
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+		if(hash1[i] != hash2[i]) {
+			return false;
+		}
+    }
+	return true;
+}
+
+int main() {
+    const char *msg = "Here is some random crazy number 437985743985743";
+
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    unsigned char our_hash[SHA256_DIGEST_LENGTH];
+
+	// Call openSSL sha256
+    SHA256((const unsigned char *)msg, strlen(msg), hash);
+	print_sha256(hash);
+
+	// Our sha256
+	sha256((const unsigned char *)msg, strlen(msg), our_hash);
+	print_sha256(our_hash);
+
+	// Test
+	if(test_sha256(hash, our_hash)) {
+		printf(BOLDGREEN "Test Passed\n");
+	} else {
+		printf(BOLDRED "Test Failed\n");
+	}
+
+    return 0;
+}
